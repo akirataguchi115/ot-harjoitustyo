1312185520cf89849ee791f9dbbb85ce150b039d
Akira Taguchi <akirahattivatti@gmail.com>
2019-11-12 10:55:55 +0200

    Tokan viikon laskarit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,42 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void kortinsaldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void lataaminenOnnistuu() {
+        kortti.lataaRahaa(10);
+        kortti.lataaRahaa(20);
+        assertEquals(40, kortti.saldo());
+    }
+    
+    @Test
+    public void ottaminenToimii1() {
+        kortti.otaRahaa(10);
+        assertEquals(0, kortti.saldo());
+    }
+    
+    @Test
+    public void ottaminenToimii2() {
+        kortti.otaRahaa(11);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void ottaminenToimii3() {
+        boolean toimii = true;
+        if (kortti.otaRahaa(10) == false) {
+            toimii = false;
+        }
+        if (kortti.otaRahaa(10) == true) {
+            toimii = false;
+        }
+        assertTrue(toimii);
     }
 }
