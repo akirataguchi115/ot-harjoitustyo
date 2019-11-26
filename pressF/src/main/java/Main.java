b1818ba5aea4772d656797e8d4ab368b4f2eee21
Akira Taguchi <akirahattivatti@gmail.com>
2019-11-26 12:40:34 +0200

    Changed main class name

--- pressF/src/main/java/Main.java
+++ pressF/src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import static javafx.application.Application.launch;
 import pressf.ui.PressFUI;
 
-public class PressF {
+public class Main {
 
     public static void main(String[] args) {
         PressFUI ui = new PressFUI();
